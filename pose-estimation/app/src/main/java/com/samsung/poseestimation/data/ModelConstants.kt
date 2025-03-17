54850550f20139491efc0152f1ca5eab7faefab8
Hubert Kang <hubert.kang@thundersoft.com>
2025-03-17 17:26:41 +0900

    posenet app merged(hubert)

--- pose-estimation/app/src/main/java/com/samsung/poseestimation/data/ModelConstants.kt
+++ pose-estimation/app/src/main/java/com/samsung/poseestimation/data/ModelConstants.kt
@@ -3,27 +3,27 @@
 package com.samsung.poseestimation.data
 
 object ModelConstants {
-    const val MODEL_NAME = "float32_pose.nnc"
+
+    const val MODEL_NAME = "posenet_mobilenet_model_simplify_O2_SingleCore.nnc"
 
     val INPUT_DATA_TYPE = DataType.FLOAT32
-    val INPUT_DATA_LAYER = LayerType.HWC
+    val INPUT_DATA_LAYER = LayerType.CHW
 
     const val INPUT_SIZE_W = 257
-    const val INPUT_SIZE_H = 257
+    const val INPUT_SIZE_H = 513
     const val INPUT_SIZE_C = 3
 
     const val INPUT_CONVERSION_SCALE = 127.5F
     const val INPUT_CONVERSION_OFFSET = 127.5F
 
     val HEATMAP_DATA_TYPE = DataType.FLOAT32
-
-    const val HEATMAP_SIZE_W = 9
-    const val HEATMAP_SIZE_H = 9
+    const val HEATMAP_SIZE_W = 17
+    const val HEATMAP_SIZE_H = 33
     const val HEATMAP_SIZE_C = 17
 
     val OFFSET_DATA_TYPE = DataType.FLOAT32
 
-    const val OFFSET_SIZE_W = 9
-    const val OFFSET_SIZE_H = 9
+    const val OFFSET_SIZE_W = 17
+    const val OFFSET_SIZE_H = 33
     const val OFFSET_SIZE_C = 34
 }
