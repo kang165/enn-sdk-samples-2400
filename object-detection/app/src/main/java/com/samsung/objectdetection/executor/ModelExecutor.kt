c96cac808d1707685cd7c401cd2738bb3689826a
Hubert Kang <hubert.kang@thundersoft.com>
2025-03-18 18:24:41 +0900

    object detection sample app merged(hubert)

--- object-detection/app/src/main/java/com/samsung/objectdetection/executor/ModelExecutor.kt
+++ object-detection/app/src/main/java/com/samsung/objectdetection/executor/ModelExecutor.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.RectF
 import android.os.SystemClock
+import android.util.Log
 import com.samsung.objectdetection.data.DataType
 import com.samsung.objectdetection.data.DetectionResult
 import com.samsung.objectdetection.data.LayerType
@@ -17,11 +18,10 @@ import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-
 @Suppress("IMPLICIT_CAST_TO_ANY")
 @OptIn(ExperimentalUnsignedTypes::class)
 class ModelExecutor(
-    var threshold: Float = 0.5F,
+    var threshold: Float = 0.3F,
     val context: Context,
     val executorListener: ExecutorListener?
 ) {
@@ -63,20 +63,19 @@ class ModelExecutor(
     }
 
     fun process(image: Bitmap) {
-        // Process Image to Input Byte Array
         val input = preProcess(image)
-        // Copy Input Data
         ennMemcpyHostToDevice(bufferSet, 0, input)
 
         var inferenceTime = SystemClock.uptimeMillis()
         // Model execute
         ennExecute(modelId)
         inferenceTime = SystemClock.uptimeMillis() - inferenceTime
-        // Copy Output Data
-        val output = ennMemcpyDeviceToHost(bufferSet, nInBuffer)
+
+        val logitsOutput = ennMemcpyDeviceToHost(bufferSet, 1)  // logits
+        val boxesOutput = ennMemcpyDeviceToHost(bufferSet, 2)  // boxes
 
         executorListener?.onResults(
-            postProcess(output), inferenceTime
+            postProcess(logitsOutput, boxesOutput), inferenceTime
         )
     }
 
@@ -91,10 +90,6 @@ class ModelExecutor(
 
     private fun preProcess(image: Bitmap): ByteArray {
         val byteArray = when (INPUT_DATA_TYPE) {
-            DataType.UINT8 -> {
-                convertBitmapToUByteArray(image, INPUT_DATA_LAYER).asByteArray()
-            }
-
             DataType.FLOAT32 -> {
                 val data = convertBitmapToFloatArray(image, INPUT_DATA_LAYER)
                 val byteBuffer = ByteBuffer.allocate(data.size * Float.SIZE_BYTES)
@@ -102,103 +97,78 @@ class ModelExecutor(
                 byteBuffer.asFloatBuffer().put(data)
                 byteBuffer.array()
             }
-
             else -> {
                 throw IllegalArgumentException("Unsupported input data type: ${INPUT_DATA_TYPE}")
             }
         }
-
         return byteArray
     }
 
-    private fun postProcess(modelOutput: ByteArray): List<DetectionResult> {
-        val output = when (OUTPUT_DATA_TYPE) {
-            DataType.UINT8 -> {
-                modelOutput.toUByteArray().map { it.toFloat() }.toFloatArray()
+    private fun postProcess(logitsOutput: ByteArray, boxesOutput: ByteArray): List<DetectionResult> {
+        val logits = ByteBuffer.wrap(logitsOutput).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer()
+        val floatLogits = FloatArray(logits.remaining())
+
+        logits.get(floatLogits)
+
+        val boxes = ByteBuffer.wrap(boxesOutput).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer()
+        val floatBoxes = FloatArray(boxes.remaining())
+        boxes.get(floatBoxes)
+
+        val result = mutableListOf<DetectionResult>()
+
+        for (i in 0 until OUTPUT_NUM_BOXES) {
+            val index = i * OUTPUT_NUM_CLASSES
+            val classProbabilities = softmax(floatLogits, index, OUTPUT_NUM_CLASSES)
+            val maxClassIndex = classProbabilities.indices.maxByOrNull { classProbabilities[it] } ?: 0
+            if (maxClassIndex == 91) {
+                Log.d("Debug", "Skipping background detection (Index: 91)")
+                continue
             }
 
-            DataType.FLOAT32 -> {
-                val byteBuffer = ByteBuffer.wrap(modelOutput).order(ByteOrder.nativeOrder())
-                val floatBuffer = byteBuffer.asFloatBuffer()
-                val floatArray = FloatArray(floatBuffer.remaining())
+            val maxClassLabel = labelList[maxClassIndex -1]
+            val maxClassScore = classProbabilities[maxClassIndex]
 
-                floatBuffer.get(floatArray)
-                floatArray
-            }
+            if (maxClassScore >= threshold) {
+                val boxIndex = i * OUTPUT_BOX_COORDS
 
-            else -> {
-                throw IllegalArgumentException("Unsupported output data type: ${OUTPUT_DATA_TYPE}")
-            }
-        }
-        val outputWithIndex = output.withIndex()
-        val result: ArrayList<DetectionResult> = arrayListOf()
+                val xCenter = floatBoxes[boxIndex]
+                val yCenter = floatBoxes[boxIndex + 1]
+                val width = floatBoxes[boxIndex + 2]
+                val height = floatBoxes[boxIndex + 3]
 
-        for (i in 0 until OUTPUT_SIZE_W) {
-            val index = i * OUTPUT_SIZE_H
-            
-            if (output[index + 4] >= threshold) {
+                val left = xCenter - width / 2
+                val top = yCenter - height / 2
+                val right = xCenter + width / 2
+                val bottom = yCenter + height / 2
+
+                if (width <= 0.02 || height <= 0.02 || left < 0 || top < 0 || right > 1 || bottom > 1) {
+                    Log.w("Debug", "Warning: Small/Invalid bounding box detected! Skipping...")
+                    continue
+                }
+
                 result.add(
                     DetectionResult(
-                        outputWithIndex
-                            .filter { it.index in index + 5 until index + OUTPUT_SIZE_H }
-                            .maxByOrNull { it.value }
-                            ?.let { Pair(labelList[it.index % OUTPUT_SIZE_H - 5], it.value) }!!,
-                        RectF(
-                            output[index + 0] - output[index + 2] / 2,
-                            output[index + 1] - output[index + 3] / 2,
-                            output[index + 0] + output[index + 2] / 2,
-                            output[index + 1] + output[index + 3] / 2
-                        )
+                        Pair(maxClassLabel, maxClassScore),
+                        RectF(left, top, right, bottom)
                     )
                 )
             }
         }
 
-        return result.filter { ((it.score.second) >= threshold) }
+        return result
     }
 
-    private fun convertBitmapToUByteArray(
-        image: Bitmap, layerType: Enum<LayerType> = LayerType.HWC
-    ): UByteArray {
-        val totalPixels = INPUT_SIZE_H * INPUT_SIZE_W
-        val pixels = IntArray(totalPixels)
-
-        image.getPixels(
-            pixels,
-            0,
-            INPUT_SIZE_W,
-            0,
-            0,
-            INPUT_SIZE_W,
-            INPUT_SIZE_H
-        )
-
-        val uByteArray = UByteArray(totalPixels * INPUT_SIZE_C)
-        val offset: IntArray
-        val stride: Int
-
-        if (layerType == LayerType.CHW) {
-            offset = intArrayOf(0, totalPixels, 2 * totalPixels)
-            stride = 1
-        } else {
-            offset = intArrayOf(0, 1, 2)
-            stride = 3
+    fun softmax(logits: FloatArray, offset: Int, length: Int): FloatArray {
+        val expValues = FloatArray(length)
+        var sum = 0f
+        for (i in 0 until length) {
+            expValues[i] = kotlin.math.exp(logits[offset + i])
+            sum += expValues[i]
         }
-
-        for (i in 0 until totalPixels) {
-            val color = pixels[i]
-            uByteArray[i * stride + offset[0]] = ((((color shr 16) and 0xFF)
-                    - INPUT_CONVERSION_OFFSET)
-                    / INPUT_CONVERSION_SCALE).toInt().toUByte()
-            uByteArray[i * stride + offset[1]] = ((((color shr 8) and 0xFF)
-                    - INPUT_CONVERSION_OFFSET)
-                    / INPUT_CONVERSION_SCALE).toInt().toUByte()
-            uByteArray[i * stride + offset[2]] = ((((color shr 0) and 0xFF)
-                    - INPUT_CONVERSION_OFFSET)
-                    / INPUT_CONVERSION_SCALE).toInt().toUByte()
+        for (i in 0 until length) {
+            expValues[i] /= sum
         }
-
-        return uByteArray
+        return expValues
     }
 
     private fun convertBitmapToFloatArray(
@@ -231,17 +201,14 @@ class ModelExecutor(
 
         for (i in 0 until totalPixels) {
             val color = pixels[i]
-            floatArray[i * stride + offset[0]] = ((((color shr 16) and 0xFF)
-                    - INPUT_CONVERSION_OFFSET)
-                    / INPUT_CONVERSION_SCALE)
-            floatArray[i * stride + offset[1]] = ((((color shr 8) and 0xFF)
-                    - INPUT_CONVERSION_OFFSET)
-                    / INPUT_CONVERSION_SCALE)
-            floatArray[i * stride + offset[2]] = ((((color shr 0) and 0xFF)
-                    - INPUT_CONVERSION_OFFSET)
-                    / INPUT_CONVERSION_SCALE)
-        }
+            val r = ((color shr 16) and 0xFF) / 255.0f
+            val g = ((color shr 8) and 0xFF) / 255.0f
+            val b = ((color shr 0) and 0xFF) / 255.0f
 
+            floatArray[i * stride + offset[0]] = (r - 0.485f) / 0.229f
+            floatArray[i * stride + offset[1]] = (g - 0.456f) / 0.224f
+            floatArray[i * stride + offset[2]] = (b - 0.406f) / 0.225f
+        }
         return floatArray
     }
 
@@ -291,13 +258,9 @@ class ModelExecutor(
         private const val INPUT_SIZE_H = ModelConstants.INPUT_SIZE_H
         private const val INPUT_SIZE_C = ModelConstants.INPUT_SIZE_C
 
-        private const val INPUT_CONVERSION_SCALE = ModelConstants.INPUT_CONVERSION_SCALE
-        private const val INPUT_CONVERSION_OFFSET = ModelConstants.INPUT_CONVERSION_OFFSET
-
-        private val OUTPUT_DATA_TYPE = ModelConstants.OUTPUT_DATA_TYPE
-
-        private const val OUTPUT_SIZE_W = ModelConstants.OUTPUT_SIZE_W
-        private const val OUTPUT_SIZE_H = ModelConstants.OUTPUT_SIZE_H
+        private const val OUTPUT_NUM_CLASSES = ModelConstants.OUTPUT_NUM_CLASSES
+        private const val OUTPUT_BOX_COORDS = ModelConstants.OUTPUT_BOX_COORDS
+        private const val OUTPUT_NUM_BOXES = ModelConstants.OUTPUT_NUM_BOXES
 
         private const val LABEL_FILE = ModelConstants.LABEL_FILE
     }
